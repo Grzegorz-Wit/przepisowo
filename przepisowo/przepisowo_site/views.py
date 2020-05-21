f3b21a083f013af129bc621d2bbd314e38872090
Grzegorz-Wit <grzegorz133@gmail.com>
2020-05-21 11:43:29 +0200

    created main template, added bootsrap and navbar

--- przepisowo/przepisowo_site/views.py
+++ przepisowo/przepisowo_site/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def
+def home_page(request):
+	context = {}
+	return render(request, 'przepisowo/home.html', context)
+
+def recipe(request):
+	context = {}
+	return render(request, 'przepisowo/recipe.html', context)
